529f49ba79c581fa9039fdcf24019222c0da83e9
kinggora <jhha09@ajou.ac.kr>
2023-10-09 17:40:09 +0900

    개발 단계 CORS 설정

---
+++ src/main/java/kinggora/portal/config/WebConfig.java
@@ -0,0 +1,22 @@
+package kinggora.portal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 개발 단계의 CORS 설정
+     * 자원 공유를 허용할 출처 origin, request methods 등
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+                .allowedMethods("*");
+    }
+}
