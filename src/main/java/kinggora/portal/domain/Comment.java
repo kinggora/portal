1334d4e8ff7210bf1c04fdd400271143e5de9237
kinggora <jhha09@ajou.ac.kr>
2023-10-24 18:48:28 +0900

    Comment 도메인 변경

--- src/main/java/kinggora/portal/domain/Comment.java
+++ src/main/java/kinggora/portal/domain/Comment.java
@@ -5,20 +5,21 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
     private Integer id;
-    private Integer boardId;
     private Integer postId;
     private Member member;
     private String content;
     private LocalDateTime regDate;
     private LocalDateTime modDate;
     private Integer ref;
+    private Integer refOrder;
     private Integer parent;
     private Integer depth;
-    private boolean hide;
+    private boolean delFlag;
 }
