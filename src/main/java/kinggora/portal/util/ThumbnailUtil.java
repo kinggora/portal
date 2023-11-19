1755e93c361a8e920d3ec053685e13c557990a0c
kinggora <jhha09@ajou.ac.kr>
2023-11-19 15:35:35 +0900

    ThumbnailUtil.createThumbnail 반환 타입 변경

--- src/main/java/kinggora/portal/util/ThumbnailUtil.java
+++ src/main/java/kinggora/portal/util/ThumbnailUtil.java
@@ -1,22 +1,20 @@
 package kinggora.portal.util;
 
-import kinggora.portal.domain.UploadFile;
-import kinggora.portal.domain.type.FileType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.UUID;
 
 @Slf4j
 @Component
 public class ThumbnailUtil {
 
-    private final static String THUMB_EXT = "jpg";
+    public final static String THUMB_EXT = "jpg";
     private final static int THUMB_WIDTH = 300;
     private final static int THUMB_HEIGHT = 300;
     private final static int RATIO = 3;
@@ -24,34 +22,22 @@ public class ThumbnailUtil {
     /**
      * 원 이미지 파일에 대한 썸네일 이미지 파일 생성
      *
-     * @param file 원 이미지 파일
-     * @return 썸네일 이미지 파일 정보
+     * @param origFile 원 이미지 파일
+     * @return 썸네일 이미지 byte array
      */
-    public UploadFile createThumbnail(UploadFile file) {
-        String storeFileName = UUID.randomUUID() + "." + THUMB_EXT;
-        File origFile = new File(file.getStoreDir(), file.getStoreName());
-        File thumbnailFile = new File(file.getStoreDir(), storeFileName);
-
+    public byte[] createThumbnail(MultipartFile origFile) {
         BufferedImage bufferedThumbImage = new BufferedImage(THUMB_WIDTH, THUMB_HEIGHT, BufferedImage.TYPE_3BYTE_BGR);
         Graphics2D graphic = bufferedThumbImage.createGraphics();
+        ByteArrayOutputStream thumbOutput = new ByteArrayOutputStream();
 
         try {
-            BufferedImage bufferedOrigImage = ImageIO.read(origFile);
+            BufferedImage bufferedOrigImage = ImageIO.read(origFile.getInputStream());
             graphic.drawImage(bufferedOrigImage, 0, 0, THUMB_WIDTH, THUMB_HEIGHT, null);
-            ImageIO.write(bufferedThumbImage, THUMB_EXT, thumbnailFile);
+            ImageIO.write(bufferedThumbImage, THUMB_EXT, thumbOutput);  //
         } catch (IOException e) {
             log.error("ThumbnailUtil.createThumbnail", e);
         }
-
-        return UploadFile.builder()
-                .postId(file.getPostId())
-                .origName(storeFileName)
-                .storeDir(file.getStoreDir())
-                .storeName(storeFileName)
-                .ext(THUMB_EXT)
-                .size(thumbnailFile.length())
-                .type(FileType.THUMBNAIL)
-                .build();
+        return thumbOutput.toByteArray();
     }
 
     private double getReducedSize(double origSize) {
